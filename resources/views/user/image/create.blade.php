726c62d4d66de6642845941c84db3abf8d18c66f
Jagadish Sharma <sharma.jagadish056@gmail.com>
2022-02-22 17:40:41 +0545

    first-commit-from-dev

--- resources/views/user/image/create.blade.php
+++ resources/views/user/image/create.blade.php
@@ -7,17 +7,18 @@
         <x-slot name="header">
             <h4 class="font-bold text-sm">Upload Image</h4>
         </x-slot>
-        <form action="{{ route('image.store') }}" method="POST" class="space-y-2" enctype="multipart/form-data">
+        <form action="{{ route('image.store') }}" method="POST" class="space-y-2.5" enctype="multipart/form-data">
             @csrf
             <div>
                 <x-form.label>Select the picture from your device</x-form.label>
-                <x-form.input type="file" id="image" name="image[]" multiple required />
+                <x-form.input type="file" id="image" name="image[]" accept=".jpeg, .jpg, .png, .gif, .bmp, .webp" multiple
+                    required />
                 @error('image')
                     <p class="invalid-feedback">{{ $message }}</p>
                 @enderror
             </div>
-            <div id="image-preview-container" class="flex flex-wrap gap-4"></div>
-            <x-button.btn class="w-full">Submit</x-button.btn>
+            <div id="image-preview-container" class="flex flex-wrap justify-center items-center gap-4"></div>
+            <x-button.btn type="submit" class="w-full uppercase">Submit</x-button.btn>
         </form>
     </x-card.lg>
 @endsection
